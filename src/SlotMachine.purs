71facceef3ab70e47b1a4fbca2e46774ff052fa2
Michael Bock <me@thought2.de>
2018-11-18 17:36:52 +0100

    style fixes

--- src/SlotMachine.purs
+++ src/SlotMachine.purs
@@ -3,29 +3,43 @@ module SlotMachine where
 import Prelude
 
 import Assets (File(File), Svg, assets_svg)
-import CSS (AnimationName(..), CSS, Color, TimingFunction(..), absolute, alignItems, animation, backgroundColor, backwards, block, borderBox, boxSizing, color, column, display, easeOut, flex, flexDirection, flexGrow, flexShrink, fontFaceFamily, fontSize, forwards, fromString, green, grid, height, hover, infinite, inlineBlock, iterationCount, justifyContent, keyframes, lineHeight, linear, margin, marginBottom, maxWidth, normalAnimationDirection, padding, paddingTop, pct, position, px, red, relative, rgb, row, sec, vh, width, with, yellow)
+import CSS (CSS)
 import CSS as CSS
+import CSS.Animation (AnimationName(..), animation, forwards, iterationCount, normalAnimationDirection)
+import CSS.Background (angular, backgroundColor, backgroundImage)
+import CSS.Border (border, borderRadius, solid)
+import CSS.Box (borderBox, boxSizing)
+import CSS.Color (Color, rgb, rgba)
 import CSS.Common as CMN
+import CSS.Display (absolute, block, display, grid, inlineBlock, position, relative, flex)
+import CSS.Flexbox (alignItems, column, flexDirection, flexGrow, flexShrink, justifyContent, row)
+import CSS.Font (bold, color, fontSize, fontWeight)
+import CSS.FontFace (fontFaceFamily)
+import CSS.Geometry (height, lineHeight, margin, marginBottom, maxWidth, padding, paddingTop, width)
+import CSS.Gradient (linearGradient)
 import CSS.Overflow (hidden, overflow, overflowAuto)
-import CSS.Selector (element)
-import CSS.Stylesheet (select)
+import CSS.Pseudo (hover)
+import CSS.Selector (element, with)
+import CSS.Size (Size, deg, pct, px, vh)
+import CSS.String (fromString)
+import CSS.Stylesheet (keyframes, select)
+import CSS.Text (letterSpacing)
 import CSS.TextAlign (textAlign, center)
+import CSS.Time (Time, sec)
+import CSS.Transition (TimingFunction(..), easeOut)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff.Random (RANDOM, randomInt)
-import Control.Monad.List.Trans (repeat)
 import Copy (Copy, CopyElem(CpLink, CpStr), copy)
-import Data.Array (concat, drop, length, range, replicate, snoc, take, (:))
+import Data.Array (concat, drop, filter, foldr, length, replicate, take)
 import Data.Enum (enumFromTo)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(..), maybe)
 import Data.Monoid (mempty)
-import Data.NonEmpty (NonEmpty(..), (:|))
+import Data.NonEmpty ((:|))
 import Data.Traversable (traverse)
-import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
 import Data.Typelevel.Num (D3)
 import Data.Vec (Vec, empty, toArray, (+>))
-import Data.Vec as Vec
 import Debug.Trace (spy)
 import Halogen (ClassName(ClassName), IProp, liftEff)
 import Halogen as H
@@ -36,16 +50,27 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Types (Link)
 
+
+moduleName :: String
+moduleName = "SlotMachine"
+
 type State =
-  { reels :: Vec D3 { from :: Int, to :: Int }
-  , extraSpins :: Int
+  { reels :: Vec D3
+      { from :: Int
+      , to :: Int
+      , extraSpins :: Int
+      , randInt :: Int
+      }
   }
 
 data Query a
   = Roll a
+  | Initialize a
 
 type IO eff =
-  (Aff (random :: RANDOM | eff))
+  Aff (random :: RANDOM | eff)
+
+--------------------------------------------------------------------------------
 
 ui :: forall eff. H.Component HH.HTML Query Unit Void (IO eff)
 ui =
@@ -57,40 +82,56 @@ ui =
     , initializer : Nothing
     , finalizer : Nothing
     }
+
+render :: State -> H.ComponentHTML Query
+render state =
+  viewMain state Nothing
+
+initialState :: State
+initialState =
+  { reels }
   where
+    reels =
+      map
+        (\i ->
+          { from : i
+          , to: i
+          , extraSpins : 0
+          , randInt : i
+          }
+        )
+        (0 +> 1 +> 2 +> empty)
 
-  initialState :: State
-  initialState =
-    { extraSpins : 0
-    , reels : 1 +> 2 +> 3 +> empty
-      # map (\i -> { from : i, to: i})
-    }
+eval :: forall eff. Query ~> H.ComponentDSL State Query Void (IO eff)
+eval = case _ of
+  Roll next -> do
+    state <- H.get
 
-  render :: State -> H.ComponentHTML Query
-  render state =
-    viewMain state Nothing
+    let n = length config.symbols
 
-  eval :: Query ~> H.ComponentDSL State Query Void (IO eff)
-  eval = case _ of
-    Roll next -> do
-      state <- H.get
+    reels <- state.reels
+      # traverse
+        (\old -> do
+          to <- randomInt 0 (n-1)
+          extraSpins <- randomInt 1 4
+          randInt <- randomInt 0 100000
+          pure
+            { from : old.to
+            , to
+            , extraSpins
+            , randInt
+            }
+        )
+      # liftEff
 
-      let n = length config.symbols
+    H.put $ state
+      { reels = reels
+      }
 
-      randVec :: Vec D3 Int <- pure (randomInt 0 (n-1))
-        # traverse id
-        # liftEff
+    pure next
 
-      let reels = Vec.zipWith
-           (\to' {to} -> { from : to, to: to' })
-           randVec
-           state.reels
-
-      H.put $ spy (state
-        { extraSpins = 1
-        , reels = reels
-        })
-      pure next
+  Initialize next -> do
+    pure next
 
 --------------------------------------------------------------------------------
 
@@ -98,7 +139,22 @@ config ::
   { symbols :: Array (File Svg)
   }
 config =
-  { symbols : enumFromTo bottom top # map assets_svg
+  { symbols : enumFromTo bottom top # map assets_svg # take 5
+  }
+
+colors ::
+  { link :: Color
+  , dark :: Color
+  , lightGrey :: Color
+  , lightYellow :: Color
+  , cta :: Color
+  }
+colors =
+  { link : rgb 175 123 124
+  , dark : rgb 53 14 15
+  , lightGrey : rgb 240 240 240
+  , lightYellow : rgb 240 240 173
+  , cta : rgb 255 181 181
   }
 
 --------------------------------------------------------------------------------
@@ -148,8 +204,7 @@ viewBody state outStyle =
       flexDirection column
       alignItems CMN.center
       justifyContent CMN.center
-      width $ pct 100.0
-      height $ pct 100.0
+      mixinFullSize
 
     styleSlot = do
       marginBottom $ vh 7.0
@@ -159,16 +214,37 @@ viewBody state outStyle =
 --------------------------------------------------------------------------------
 
 viewSlot :: State -> Maybe CSS -> H.ComponentHTML Query
-viewSlot { reels, extraSpins } outStyle =
+viewSlot { reels } outStyle =
   HH.div
     [ nameIt "viewSlot"
     , combineStyles style outStyle
     ]
     ( map
-        (\{ from, to } -> viewReel { from, to, extraSpins } Nothing)
-        (toArray reels)
+        (\{ from, to, extraSpins, randInt } ->
+           viewReel
+             { from
+             , to
+             , extraSpins
+             , occurences : occurences to (map _.to reelsArray)
+             , maxSpinPathLength
+             , randInt
+             }
+             Nothing
+        )
+        reelsArray
     )
   where
+    reelsArray = toArray reels
+
+    maxSpinPathLength =
+      map
+        (\{from, to, extraSpins} ->
+          getSpinPath from to extraSpins reelsArray
+          # length
+        )
+        reelsArray
+      # foldr max 0
+
     style = do
       display flex
       flexDirection row
@@ -178,101 +254,168 @@ viewSlot { reels, extraSpins } outStyle =
 --------------------------------------------------------------------------------
 
 viewReel ::
-  { from :: Int, to :: Int, extraSpins :: Int }
+  { from :: Int
+  , to :: Int
+  , extraSpins :: Int
+  , occurences :: Int
+  , maxSpinPathLength :: Int
+  , randInt :: Int
+  }
   -> Maybe CSS
   -> H.ComponentHTML Query
-viewReel { from, to, extraSpins } outStyle =
+viewReel { from, to, extraSpins, occurences, maxSpinPathLength, randInt } outStyle =
   viewBoxAspect
     { aspect : 1.0 }
     (Just style)
-    (HH.div
-       [ HCSS.style styleReel ]
-       ( map
-           (\sym -> viewSvg sym (Just styleItem))
-           spinPath
-         # \xs -> snoc xs (HCSS.stylesheet sheet)
-       )
+    ( HH.div
+        [ HCSS.style styleWrapper ]
+        [ HH.div
+            [ HCSS.style styleReel ]
+            (map viewReelItem spinPath <> [ HCSS.stylesheet sheetReel ])
+        , HH.div [HCSS.style styleBlend] [  ]
+        , HCSS.stylesheet sheet
+        ]
     )
   where
+    styleWrapper = do
+      mixinFullSize
+
     spinPath = config.symbols
       # getSpinPath from to extraSpins
 
+    animName = "anim" <> show randInt
+
     style = do
       overflow hidden
-
-    animName = "anim" <> show from <> show to
-
-    styleReel = do
-      boxSizing borderBox
-      width (pct 100.0)
-      height (pct 100.0)
-      display flex
-      flexDirection column
-
-    styleItem = do
-      flexGrow 0
-      flexShrink 0
-      margin (pct 5.0) (pct 5.0) (pct 5.0) (pct 5.0)
-      animation
-        (AnimationName $ fromString animName)
-        (sec $ toNumber (length spinPath) * 0.1)
-        (TimingFunction $ fromString "ease-in-out")
-        (sec 0.0)
-        (iterationCount 1.0)
-        normalAnimationDirection
-        backwards
+      backgroundColor colors.lightGrey
+      borderRadius (pct 15.0) (pct 15.0) (pct 15.0) (pct 15.0)
+      margin (pct 2.0) (pct 2.0) (pct 2.0) (pct 2.0)
+      if occurences > 1
+         then mixinDefaultAnimation
+           { name : animName
+           , timingFunction : easeInOut
+           , duration : sec 1.0
+           , delay : maxSpinDuration
+           }
+         else pure mempty
 
     sheet = do
-      let pctY = -100.0 * toNumber (length spinPath - 1)
-          transformTranslateY n =
-            prop "transform" $ "translateY(" <> show n <> "%)"
       keyframes
         animName
-        ( (0.0 /\ do transformTranslateY pctY) :|
-          [ (80.0 /\ do transformTranslateY 20.0)
-          , (100.0 /\ do transformTranslateY 0.0)
+        ( (0.0 /\ do
+             prop "transform" $ "rotateY(" <> show 0.0 <> "deg)"
+          ) :|
+          [ (100.0 /\ do
+               prop "transform" $ "rotateY(" <> show 360.0 <> "deg)"
+               backgroundColor colors.lightYellow
+            )
           ]
         )
 
+    styleBlend = do
+      let transparent = rgba zero zero zero zero
+      backgroundImage $ linearGradient (angular $ deg zero)
+        [ (colors.lightGrey /\ pct 0.0)
+        , (transparent /\ pct 5.0)
+        , (transparent /\ pct 90.0)
+        , (colors.lightGrey /\ pct 100.0)
+        ]
+      position absolute
+      mixinStickToEdges
 
+    animNameReel = "animReel" <> show randInt
 
-viewReel' :: { index :: Int } -> Maybe CSS -> H.ComponentHTML Query
-viewReel' { index } outStyle =
-  viewBoxAspect
-    { aspect : 1.0 }
-    Nothing
-    ( HH.div
-        [HCSS.style styleReel]
-        [HH.text $ show index]
-    )
-  where
+    maxSpinDuration = sec $ (toNumber maxSpinPathLength * 0.3) + (spy 2.0)
+
+    spinDuration = sec $ toNumber (length spinPath) * 0.3
+
     styleReel = do
       boxSizing borderBox
-      width (pct 100.0)
-      height (pct 100.0)
+      mixinFullSize
+      display flex
+      flexDirection column
+
+      mixinDefaultAnimation
+        { name : animNameReel
+        , timingFunction : easeOut
+        , duration : spinDuration
+        , delay : sec zero
+        }
+
+    sheetReel = do
+      let
+        pctY = -100.0 * toNumber (length spinPath - 1)
+      keyframes
+        animNameReel
+        ( (0.0 /\ do
+             mixinTransformTranslateY 0.0
+          ) :|
+          [ (90.0 /\ do
+               mixinTransformTranslateY $ pctY - 30.0
+            )
+          , (100.0 /\ do
+               mixinTransformTranslateY pctY
+            )
+          ]
+        )
 
 --------------------------------------------------------------------------------
 
-viewButton :: { onClick :: Unit -> Query Unit } -> Maybe CSS -> H.ComponentHTML Query
+viewReelItem :: File Svg -> H.ComponentHTML Query
+viewReelItem sym =
+  viewBoxAspect
+    { aspect : 1.0 }
+    (Just style)
+    (viewSvg sym (Just styleImg))
+  where
+    style = do
+      width (pct 100.0)
+      height (pct 100.0)
+
+    styleImg =  do
+      flexGrow 0
+      flexShrink 0
+      margin (pct 10.0) (pct 10.0) (pct 10.0) (pct 10.0)
+
+--------------------------------------------------------------------------------
+
+viewButton ::
+  { onClick :: Unit -> Query Unit }
+  -> Maybe CSS
+  -> H.ComponentHTML Query
 viewButton { onClick } outStyle =
-  HH.div
+  HH.button
     [ nameIt name
     , combineStyles style outStyle
     , HE.onClick (input_ onClick)
     ]
-    [ HH.text "AAA"
+    [ HH.text "GO"
     , HCSS.stylesheet sheet
     ]
   where
     name = "viewButton"
 
-    sheet = select (with (element $ getClassStr name) hover) do
-      backgroundColor red
+    sheet = do
+      select (with (element $ getClassStr name) hover) do
+        backgroundColor colors.lightGrey
+      select (element $ getClassStr name) do
+        backgroundColor colors.cta
+      select (element $ getClassStr name <> ":focus") do
+        prop "outline" "0"
 
     style = do
-      maxWidth $ px 240.0
+      width $ px 200.0
       height $ px 40.0
       prop "cursor" "pointer"
+      fontFaceFamily "Arial"
+      fontSize $ px 20.0
+      color colors.dark
+      fontWeight bold
+      mixinBorderRadius (px 15.0)
+      letterSpacing (px 5.0)
+      border solid (px 3.0) colors.lightYellow
+
+
 
 --------------------------------------------------------------------------------
 
@@ -355,28 +498,13 @@ viewBoxAspect { aspect } outStyle children =
 
     styleContent = do
       position absolute
-      CSS.top $ px zero
-      CSS.left $ px zero
-      CSS.right $ px zero
-      CSS.bottom $ px zero
+      mixinStickToEdges
 
 --------------------------------------------------------------------------------
-
-colors ::
-  { link :: Color
-  , dark :: Color
-  }
-colors =
-  { link : rgb 175 123 124
-  , dark : rgb 53 14 15
-  }
 
 nameIt :: forall a b. String -> IProp ( "class" :: String | b ) a
 nameIt str =
   HP.class_ (ClassName $ moduleName <> "_" <> str)
-
-moduleName :: String
-moduleName = "SlotMachine"
 
 combineStyles :: forall a b. CSS -> Maybe CSS -> IProp ( style :: String | a ) b
 combineStyles style maybeStyle =
@@ -406,3 +534,49 @@ getSpinPath from to extraSpins xs =
       <> take (to + 1) xs
        # replicate extraSpins
        # concat
+
+occurences :: forall a. Eq a => a -> Array a -> Int
+occurences x xs =
+  filter (_ == x) xs # length
+
+--------------------------------------------------------------------------------
+
+mixinTransformTranslateY :: Number -> CSS
+mixinTransformTranslateY n =
+  prop "transform" $ "translateY(" <> show n <> "%)"
+
+mixinDefaultAnimation ::
+  { timingFunction :: TimingFunction
+  , duration :: Time
+  , delay :: Time
+  , name :: String
+  }
+  -> CSS
+mixinDefaultAnimation { name, duration, timingFunction, delay } =
+  animation
+    (AnimationName $ fromString name)
+    duration
+    timingFunction
+    delay
+    (iterationCount 1.0)
+    normalAnimationDirection
+    forwards
+
+mixinStickToEdges :: CSS
+mixinStickToEdges = do
+  CSS.top $ px zero
+  CSS.left $ px zero
+  CSS.right $ px zero
+  CSS.bottom $ px zero
+
+mixinFullSize :: CSS
+mixinFullSize = do
+  width (pct 100.0)
+  height (pct 100.0)
+
+easeInOut :: TimingFunction
+easeInOut = TimingFunction $ fromString "ease-in-out"
+
+mixinBorderRadius :: forall a. Size a -> CSS
+mixinBorderRadius s = do
+  borderRadius s s s s
